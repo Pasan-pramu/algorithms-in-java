68417994836311bce2e02454da74b1454c7551fd
Pasan24 <pramudithapasan076@gmail.com>
2025-09-19 23:13:47 +0530

    updated the bubllesort  algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 
+
 public class Main {
     public static void main(String[] args) {
+
+        // Bubble Sort Test
+        int[] bubbleArr = {64, 25, 12, 22, 11};
+        System.out.println("Before Bubble Sort:");
+        printArray(bubbleArr);
+        BubbleSort.sort(bubbleArr);
+        System.out.println("After Bubble Sort:");
+        printArray(bubbleArr);
+
+    }
+        // Utility method to print arrays
+        private static void printArray(int[] arr) {
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+
 
     }
 }
