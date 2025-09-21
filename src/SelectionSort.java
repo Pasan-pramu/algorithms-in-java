1b171acd89ca96bc8c0b2d36bbc3a696547b1321
Pasan24 <pramudithapasan076@gmail.com>
2025-09-21 21:58:44 +0530

    Find minimum index & swap

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -7,6 +7,20 @@ public class SelectionSort {
         for(int i =0 ; i < n-1 ; i++){
             // Find the minimum element in the remaining array
 
+            int minIndex =i ;
+
+
+            for(int j=i+1 ;j < n ; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j ;
+                }
+            }
+
+            // Swap arr[minIndex] and arr[i]
+            int temp = arr[minIndex];
+            arr[minIndex] =arr[i];
+            arr[i] = temp;
+
         }
 
     }
