985266b8fec315f58fb834821c87c0d0be5f4aef
Pasan24 <pramudithapasan076@gmail.com>
2025-09-21 21:50:13 +0530

    Implement outer loop

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -2,5 +2,12 @@ public class SelectionSort {
     public static void sort(int[] arr) {
         // Sorting logic will go here
 
+        int n = arr.length;
+
+        for(int i =0 ; i < n-1 ; i++){
+            // Find the minimum element in the remaining array
+
+        }
+
     }
 }
